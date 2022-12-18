3a4378fb3f5fc1dc31ba1a8fd8e02e3746e13034
Sairam Kaushik <sairamkaushik1@gmail.com>
2022-12-18 18:02:38 +0530

    modified hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import banner from "../assets/images/hero.jpg";
+import product1 from "../assets/images/alloySteelWire.webp";
+import product2 from "../assets/images/nonAlloy.jpg";
+import { Link } from "react-router-dom";
 export const Hero = () => {
   return (
-    <div>
-        This is hero section
-    </div>
-  )
-}
+    <>
+      <section className="min-h-screen flex py-10 md:flex-row flex-col items-center">
+        <div className="flex-1 flex md:text-left text-center md:ml-10 md:mt-5 mt-10 flex-col m-5">
+          <h1 className="md:text-7xl text-3xl font-bold text-[#d7e3ef]">
+            Welcome to
+            <p>Shri Chari Wires</p>
+          </h1>
+          <p className="text-lg text-[#d7e3ef] mt-4">
+            Shri Chari Wires, our flag ship group company, has been established
+            in 1995. It is located at 23/7, Mathura Road, Sector-58,
+            Faridabad-121004. It is 60 Km from Indira Gandhi International
+            airport & 50 Kms form New Delhi Railway Station.
+          </p>
+          <p className="text-lg text-[#d7e3ef] mt-4">
+            Shri Chari Wires now having Annealing capacity of 15000 MT per year,
+            Drawing Capacity of 20000 MT per year and Size Range 1.80 to 36.00
+            MM, produces world class steel wires in various grades like low
+            carbon, medium carbon, C-Mn Steel, Boron Steels and Alloy Steel
+            Wires. These steel wires are manufactured to suit varied needs of
+            engineering, automobile & allied industries.
+          </p>
+        </div>
+        <div className="flex-1">
+          <div className="flex h-full">
+            <img
+              src={banner}
+              className="md:w-[600px] w-[300px] md:ml-20 rounded-3xl border-4 border-[#d7e3ef]"
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="bg-[#d7e3ef] w-full md:h-[50vh] ">
+        <div className="flex flex-col m-5 items-center justify-center">
+          <h2 className="md:text-5xl text-3xl md:text-center text-center font-bold m-5 ">Our Products</h2>
+          <div className="flex md:flex-row flex-col md:gap-20 gap-10 mt-8">
+            <div className="flex flex-col gap-4 text-center">
+              <img
+                src={product1}
+                className="md:w-[200px] w-[150px] rounded-full border-4 border-[#0b131f]"
+              />
+              <Link to="/alloy_steel_wire" className="text-lg font-semibold">Allow Steel Wires</Link>
+            </div>
+
+            <div className="flex flex-col gap-4 text-center">
+              <img
+                src={product2}
+                className="md:w-[200px] w-[150px] rounded-full border-4 border-[#0b131f]"
+              />
+              <Link to="/non_alloy_steel_wire" className="text-lg font-semibold">Non-Allow Steel Wires</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { CgMenuRight } from 'react-icons/cg'
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 export const Navbar = () => {
@@ -43,26 +44,38 @@ export const Navbar = () => {
     })
 })
 
-  return (
-    <div className="flex justify-center">
-      <div className="fixed w-[90vw] rounded-tr-3xl rounded-tl-3xl h-[10vh]  bg-[#0b131f]">
-        <div className="flex items-center justify-between">
-          <div className="m-5">
-            <img src={logo} className="md:w-[300px] w-[200px]" />
+return (
+  <nav className={`fixed w-full left-0 top-0 z-[999] rounded-br-3xl rounded-bl-3xl ${sticky ? "bg-[#0b131f] text-white" : "text-white"}`}>
+      <div className='flex item-center justify-between'>
+          <div className='mx-7'>
+              <img src={logo} className="md:w-[300px] w-[250px] md:mt-4 mt-4"/>
           </div>
-          <div className="text-white md:block hidden px-7 py-2 font-medium rounded-bl-full cursor-pointer">
-            <ul className="flex items-center gap-1 py-2 text-lg">
-                {
-                    menuLinks?.map((menu,i) =>(
-                        <li key={i} className='px-6 hover:text-cyan-600'>
-                            <Link to={menu?.link}>{menu?.name}</Link>
-                        </li>
-                    )) 
-                }
-            </ul>
+          <div className={` ${sticky ? 'md:bg-white/0 bg-[#0b131f]' : 'bg-[#0b131f]'} text-gray-900 md:block hidden px-7 py-2 font-medium rounded-bl-full`}>
+              <ul className='flex items-center gap-1 py-2 text-lg'>
+                  {
+                      menuLinks?.map((menu, i) => (
+                          <li key={i} className='px-6 text-white hover:text-cyan-600'>
+                              <Link to={menu?.link}>{menu?.name}</Link>
+                          </li>
+                      ))
+                  }
+              </ul>
           </div>
-        </div>
+          <div onClick={() => setOpen(!open)} className={`z-[999] ${open ? "text-gray-900" : "text-gray-800"} text-3xl md:hidden m-5`}>
+              <CgMenuRight />
+          </div>
+          <div className={`md:hidden text-gray-900 absolute w-2/2 h-screen px-3 py-2 font-medium bg-white top-0  duration-1000 ${open ? 'right-0' : 'right-[-100%]'}`}>
+              <ul className='flex flex-col justify-center h-full gap-10 py-2 text-lg'>
+                  {
+                      menuLinks?.map((menu, i) => (
+                          <li key={i} className='px-6 hover:text-cyan-600'>
+                              <Link to={menu?.link}>{menu?.name}</Link>
+                          </li>
+                      ))
+                  }
+              </ul>
+          </div>
       </div>
-    </div>
-  );
+  </nav>
+)
 };
